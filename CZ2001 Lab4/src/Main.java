b86639ad7f14a95653b1b378db4a1b7619c73234
LeonardisOne <32675659+LeonardisOne@users.noreply.github.com>
2018-11-02 19:13:01 +0800

    Change adjacentCity from ArrayList to Array

--- CZ2001 Lab4/src/Main.java
+++ CZ2001 Lab4/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Main {
 
-	public static void BFS(int source, int destination, List<LinkedList<Integer>> adjacentCity, Stack<Integer> way,int numOfCity)
+	public static void BFS(int source, int destination, LinkedList<Integer>[] adjacentCity, Stack<Integer> way,int numOfCity)
 	{
 		List<Boolean> visited = new ArrayList<>();
 		List<Integer> prev = new ArrayList<>();
@@ -22,7 +22,7 @@ public class Main {
 		while (!queue.isEmpty() && curr!=destination)
 		{
 			curr=queue.poll();
-			LinkedList<Integer> adj=adjacentCity.get(curr);
+			LinkedList<Integer> adj=adjacentCity[curr];
 			for (Integer i:adj)
 			{
 				if (visited.get(i)== false)
@@ -43,24 +43,24 @@ public class Main {
 		way.push(source);
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc=new Scanner(System.in);
 		while (true)
 		{
-			System.out.println("Enter the number of city:");
+			System.out.println("Enter the number of cities:");
 			int numOfCity = sc.nextInt();
-			List<LinkedList<Integer>> adjacentCity= new ArrayList<LinkedList<Integer>>();
+			LinkedList<Integer>[] adjacentCity= new LinkedList[numOfCity];
 			for (int i=0;i<numOfCity;i++)
 			{
 				LinkedList<Integer> adjacent= new LinkedList<Integer>();
-				adjacentCity.add(adjacent);
+				adjacentCity[i]=adjacent;
 			}
 			String[]cityName=CityNameGenerator.generateCityNames("src/cities_1000.txt",numOfCity);
 			
-			//conditions for the min and max number of edges, based on the number of nodes(numOfCity)
-			System.out.println("Enter the number of edge("+(numOfCity-1)+"-"+(((numOfCity*(numOfCity-1)/2))-1)+")\t:");
 			int numOfEdge;
 			do {
+				//conditions for the min and max number of edges, based on the number of nodes(numOfCity)
+				System.out.println("Enter the number of edge("+(numOfCity-1)+"-"+(((numOfCity*(numOfCity-1)/2))-1)+")\t:");
 				numOfEdge=sc.nextInt();
 			} while (numOfEdge<(numOfCity-1) || numOfEdge>(numOfCity)*((numOfCity-1/2)-1));
 
@@ -71,8 +71,8 @@ public class Main {
 			{
 				if (i>0)
 				{
-					adjacentCity.get(i).add(i-1);
-					adjacentCity.get(i-1).add(i);
+					adjacentCity[i].add(i-1);
+					adjacentCity[i].add(i);
 				}
 			}
 
@@ -82,20 +82,20 @@ public class Main {
 				int v2=random.nextInt(numOfCity);
 
 				//if the vertices index are the same, or only differs by 1, or has already had edges between them, continue looping
-				if (Math.abs((v1-v2))<=1|| adjacentCity.get(v1).contains(v2))
+				if (Math.abs((v1-v2))<=1|| adjacentCity[v1].contains(v2))
 					i--;
 
 				//otherwise, add it into the graph
 				else
 				{
-					adjacentCity.get(v1).add(v2);
-					adjacentCity.get(v2).add(v1);
+					adjacentCity[v1].add(v2);
+					adjacentCity[v2].add(v1);
 				}
 			}
 			for (int i=0;i<numOfCity;i++)
 			{
 				System.out.println("["+i+"]\t"+cityName[i]);
-				System.out.println(i+"\t"+adjacentCity.get(i));
+				System.out.println(i+"\t"+adjacentCity[i]);
 			}
 
 			int source=-1,destination=-1;
